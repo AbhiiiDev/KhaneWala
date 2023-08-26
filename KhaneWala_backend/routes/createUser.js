d73a198357208b6f1ee7809048f382ba20f1f71d
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-08-27 01:14:31 +0530

    bcrypt-done#ErrorPresent

--- KhaneWala_backend/routes/createUser.js
+++ KhaneWala_backend/routes/createUser.js
@@ -4,13 +4,21 @@ const router=express.Router()
 
 const User=require("../models/User")
 
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const { Await } = require('react-router-dom');
+
+const jwtSecret="heythisisjwtsecretnoonecanbreakthis"
+
 router.post("/createUser",async(req,res)=>{
 
+    const salt=await bcrypt.genSalt(10);
+    let secPass=await bcrypt.hash(req.body.password,salt);
 
     try {
      await  User.create({
            name:req.body.name,
-           password:req.body.password,
+           password:secPass,
            email:req.body.email,
            location:req.body.location,
         })
@@ -35,14 +43,25 @@ let email=req.body.email;
         errors:"No User registered"
     })
   }
- if(!req.body.password === userData.password)
+console.log(userData)
+  const pwCompare= await bcrypt.compare(req.body.password,userData.password);
+ if(!pwCompare)
  {
     return res.status(400).json({
         errors:"Enter correct credentials"
     })
  }
+//it is mandatory for data to be object to get signed with jwt
+const data={
+    user:{
+        id:userData.id
+    }
+}
+
+const authToken=jwt.sign(data,jwtSecret)
 return res.json({
-    success:true
+    success:true,
+    authToken:authToken
 })
     } catch (error) {
         console.log(error)
