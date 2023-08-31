c11f2cc63f443b4042c94c93205e6e93b4d89b0a
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-08-31 22:11:19 +0530

    Radom-Commit

--- KhaneWala/src/screens/Home.jsx
+++ KhaneWala/src/screens/Home.jsx
@@ -3,11 +3,32 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
 import Carousel from '../components/Carousel'
+import { useEffect, useState } from 'react'
 
 
 const Home = () => {
 
 
+const [foodCat, setfoodCat] = useState([]);
+const [foodItem, setFoodItem]=useState([]);
+
+const loadData=async()=>{
+  let response=await fetch("http://localhost:5000/api/foodData",{
+    method:"POST",
+    headers:{
+      "Content-Type":'application/json'
+    }
+  }
+  )
+;
+  response=await response.json();
+  console.log(response[0],response[1])
+}
+
+
+useEffect(()=>{
+  loadData();
+},[])
 
   return (
  
@@ -16,10 +37,19 @@ const Home = () => {
      <Navbar/> 
     </div>
   <div><Carousel/></div>
-    <div>
-      <Card/>
-     <Card/>
-     <Card/>
+    <div className='container'>
+
+
+      {
+        foodCat !==[]
+        ? foodCat.map((data,index)=>{
+          return (
+            <div> {data }</div>
+          )
+        })
+        : "********"
+      }
+      
     </div>
 
     <div><Footer/></div>
