fd11060ff2c204b42123dcca22d53a980c4f3b53
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-09-02 00:37:53 +0530

    working-on-showing-data-to-frontEnd

--- KhaneWala/src/screens/Home.jsx
+++ KhaneWala/src/screens/Home.jsx
@@ -1,61 +1,77 @@
-
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Card from '../components/Card'
-import Carousel from '../components/Carousel'
-import { useEffect, useState } from 'react'
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [foodCat, setfoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
 
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
 
-const [foodCat, setfoodCat] = useState([]);
-const [foodItem, setFoodItem]=useState([]);
+    setfoodCat(response[1]);
+    setFoodItem(response[0]);
+    console.log(response[0], response[1]);
+  };
 
-const loadData=async()=>{
-  let response=await fetch("http://localhost:5000/api/foodData",{
-    method:"POST",
-    headers:{
-      "Content-Type":'application/json'
-    }
-  }
-  )
-;
-  response=await response.json();
-  console.log(response[0],response[1])
-}
-
-
-useEffect(()=>{
-  loadData();
-},[])
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Carousel />
+      </div>
+      <div className="container">
+        {foodCat !== []
+          ? foodCat.map((data) => {
+              return (  
+                <div className="row mb-3">
+                <div key={data._id} className="fs-3 m-3">
+                  {data.CategoryName}
+                </div> 
+               <hr/>
+               {
+            foodItem !==[]
+            ?
+            foodItem.filter((item)=> item.CategoryName===data.CategoryName).map(filterItems=>{
+              return (
+                <div key={filterItems._id} className="col-12 col-md-6 col-lg-3 ">
+            <Card/>
+            </div>
+              )
+            }):
+            <div>
+              "No such data found" 
+                </div> 
+        } 
+
+                </div>
+
+
+              );
+            })
+          : ""}
  
-          <>
-    <div>
-     <Navbar/> 
-    </div>
-  <div><Carousel/></div>
-    <div className='container'>
+      </div>
 
-
-      {
-        foodCat !==[]
-        ? foodCat.map((data,index)=>{
-          return (
-            <div> {data }</div>
-          )
-        })
-        : "********"
-      }
-      
-    </div>
-
-    <div><Footer/></div>
+      <div>
+        <Footer />
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
