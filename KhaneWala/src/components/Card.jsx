329b69a7f760de1c8b529b4c0fef7903e825d3e9
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-09-02 23:17:24 +0530

    Card_real-data-implementd

--- KhaneWala/src/components/Card.jsx
+++ KhaneWala/src/components/Card.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+
+let options=props.options;
+let priceOptions=Object.keys(options);
+
+
   return (
   <div>
         <div className="card m-3" style={{ width: "18rem" }}>
-          <img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
+          <img src={props.imgSrc} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Demo food text is this</p>
+            <h5 className="card-title">{props.foodName}</h5>
+           
 
             <div className="container w-100">
               <select className="m-2 h-100 bg-dark rounded ">
@@ -20,8 +25,15 @@ export default function Card() {
                 })}
               </select>
               <select className="m-2 h-100 bg-dark rounded ">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
+           {
+            priceOptions.map(
+              (data)=>{
+                return <option key={data} value={data}>
+                  {data}
+                </option>
+              }
+            )
+           }
               </select>
 
               <div className="d-inline h-100 fs-5">Total Price</div>
