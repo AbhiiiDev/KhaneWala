b318074bf07bae793a39a611a3792c1269609123
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-09-14 20:15:50 +0530

    Add-To-Cart#with-bugs

--- KhaneWala/src/components/Card.jsx
+++ KhaneWala/src/components/Card.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React,{useEffect, useRef, useState} from 'react'
+import {useCart,useDispatchCart} from './ContextReducer';
+import PropTypes from 'prop-types';
+Card.propTypes = {
+  foodItems: PropTypes.object.isRequired, // You can adjust the PropTypes based on the actual data type
+  options: PropTypes.any, // Define the correct PropTypes for options
+};
+
 
 export default function Card(props) {
+ let dispatch=useDispatchCart();
+  let data=useCart();
+  let priceRef=useRef();
+  let options=props.options;
+  let priceOptions=Object.keys(options);
+const [qty, setQty] = useState(1)
+const [size, setSize] = useState("")
 
+const handleAddToCart=async()=>{
+  await dispatch({type:"Add",id:props.foodItems._id,name:props.foodItems.name,price:finalPrice,img:props.foodItems.img,qty:qty,size:size})
+  await console.log(data);
+  }
+  let finalPrice=qty* parseInt(options[size]);
 
-const handleAddToCart=()=>{
+useEffect(()=>{
+setSize(priceRef.current.value)
+},[])
 
-}
-
-
-let options=props.options;
-let priceOptions=Object.keys(options);
 
 
   return (
   <div>
         <div className="card m-3" style={{ width: "18rem" }}>
-          <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"150px",objectFit:"fill"}} />
+          <img src={props.foodItems.img} className="card-img-top" alt="..." style={{height:"150px",objectFit:"fill"}} />
           <div className="card-body">
-            <h5 className="card-title">{props.foodName}</h5>
+            <h5 className="card-title">{props.foodItems.name}</h5>
            
 
             <div className="container w-100">
-              <select className="m-2 h-100 bg-success rounded ">
+              <select className="m-2 h-100 bg-success rounded " onChange={(e)=>setQty(e.target.value)}>
                 {Array.from(Array(6), (e, i) => {
                   return (
                     <option key={i + 1} value={i + 1}>
@@ -30,7 +46,7 @@ let priceOptions=Object.keys(options);
                   );
                 })}
               </select>
-              <select className="m-2 h-100 bg-success rounded ">
+              <select className="m-2 h-100 bg-success rounded " ref={priceRef} onChange={(e)=>setSize(e.target.value)}>
            {
             priceOptions.map(
               (data)=>{
@@ -42,7 +58,9 @@ let priceOptions=Object.keys(options);
            }
               </select>
 
-              <div className="d-inline h-100 fs-5">Total Price</div>
+              <div className="d-inline h-100 fs-5">
+                Rs.{finalPrice}
+              </div>
             </div>
           <hr/>
           <button className={`btn btn-success justify-center ms-2`} onClick={handleAddToCart} >
