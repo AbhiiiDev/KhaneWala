de78728e7647981d7c40f6b077f9d3c74f784215
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-09-24 00:49:10 +0530

    modal-updated

--- KhaneWala/src/modal.jsx
+++ KhaneWala/src/modal.jsx
@@ -5,11 +5,12 @@ const MODAL_STYLES = {
   position: 'fixed',
   top: '50%',
   left: '50%',
-  backgroundColor: 'rgb(34,34,34)',
+  backgroundColor: 'rgb(249, 245, 245)',
   transform: 'translate(-50%, -50%)',
   zIndex: 1000,
   height: '90%',
-  width: '90%'
+  width: '90%',
+  
 }
 
 const OVERLAY_STYLES = {
@@ -18,8 +19,8 @@ const OVERLAY_STYLES = {
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 1000
+  backgroundColor: 'rgba(15, 15, 15, 0.7)',
+  zIndex: 1000,
 }
 
 export default function Modal({ children, onClose }) {
