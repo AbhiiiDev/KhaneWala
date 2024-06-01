1967d1e47b3aec7f6188bc7289fdf5d88fab64b4
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-06-01 18:36:24 +0530

    Frontend setup complete with ShadCN and tailwindCSS

---
+++ KhaneWala/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from './components/ui/button'
+import './global.css'
+function App() {
+
+  return (
+    <>
+     <Button>
+      Click me
+     </Button>
+  
+    </>
+  )
+}
+
+export default App
