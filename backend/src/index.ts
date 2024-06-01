58d833fc243b9a36278515d78882ec6a138f3712
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-06-01 19:05:03 +0530

    MongoDb connection successfull

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI as string).then(()=>{console.log('database connected successfully')});
+
 
 app.get("/test", (req, res) => {
   res.json({
