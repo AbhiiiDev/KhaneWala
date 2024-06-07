93afcb570090972a034e2633decc3e2b233b3609
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-06-07 18:19:25 +0530

    User update form added

---
+++ KhaneWala/src/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,121 @@
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import React from 'react'
+import {useForm} from 'react-hook-form'
+import {z} from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '@/components/ui/input';
+import LoadingButton from '../components/LoadingButton';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1,"name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+});
+
+export type UserFormData=z.infer<typeof formSchema>;
+
+ type Props = {
+//   currentUser: User;
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+//   title?: string;
+//   buttonText?: string;
+ };
+
+export default function UserProfileForm({title='User Profile',onSave,isLoading}:Props) {
+
+  const form=useForm<UserFormData>({
+    resolver:zodResolver(formSchema)
+  })
+
+  return (
+
+<Form {...form}>
+  <form onSubmit={form.handleSubmit(onSave)}  className="space-y-4 bg-gray-50 rounded-lg md:p-10">
+  <div>
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+
+  <FormField
+    control={form.control}
+    name="email"
+    render={({field}) => (
+      <FormItem>
+        <FormLabel>Email</FormLabel>
+        <FormControl>
+     <Input {...field} disabled className='bg-white' />
+        </FormControl>
+      </FormItem>
+    )}
+  />
+  <FormField
+    control={form.control}
+    name="name"
+    render={({field}) => (
+      <FormItem>
+        <FormLabel>Name</FormLabel>
+        <FormControl>
+     <Input {...field} className='bg-white' />
+        </FormControl>
+      </FormItem>
+    )}
+  />
+
+  <div className='flex flex-col md:flex-row gap-4'>
+
+  
+  <FormField
+    control={form.control}
+    name="addressLine1"
+    render={({field}) => (
+      <FormItem className='flex-1'>
+        <FormLabel>AddressLine1</FormLabel>
+        <FormControl>
+     <Input {...field}  className='bg-white' />
+        </FormControl>
+      </FormItem>
+    )}
+  />
+  <FormField
+    control={form.control}
+    name="city"
+    render={({field}) => (
+      <FormItem className='flex-1'>
+        <FormLabel>City</FormLabel>
+        <FormControl>
+     <Input {...field} className='bg-white' />
+        </FormControl>
+      </FormItem>
+    )}
+  />
+  <FormField
+    control={form.control}
+    name="country"
+    render={({field}) => (
+      <FormItem className='flex-1'>
+        <FormLabel>Country</FormLabel>
+        <FormControl>
+     <Input {...field} className='bg-white' />
+        </FormControl>
+      </FormItem>
+    )}
+  />
+  </div>
+  {
+    isLoading ? (<LoadingButton/>):
+    (
+    <Button>
+      Update
+    </Button>
+    )
+  }
+  </form>
+</Form>
+  )
+}
