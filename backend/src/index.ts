54a2bb45cf3b6fde3afc93d0154e781719dab314
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2024-07-02 00:30:22 +0530

    Installed checkbox from shadCN

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import mongoose from "mongoose";
+import mongoose,{ConnectOptions} from "mongoose";
 import UserRoute from './routes/UserRoute';
 import RestaurantRoute from "./routes/RestaurantRoute";
 import {v2 as cloudinary} from 'cloudinary'
-
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI as string).then(()=>{console.log('database connected successfully')});
+
+mongoose.connect(process.env.MONGO_URI as string).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 cloudinary.config({
   cloud_name:process.env.CLOUD_NAME,
@@ -21,13 +25,11 @@ cloudinary.config({
 });
 
 
-
 app.get("/test", (req, res) => {  
   res.json({
     message: "test server working fine",
   });
 });
-
 
 app.use('/api/v1/user',UserRoute);
 app.use('/api/v1/restaurant',RestaurantRoute)
