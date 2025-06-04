9a4b8b2ae7a947c31a4dcb43e2d3f77fc41c3757
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2025-06-04 13:39:37 +0530

    added persisted store to the app

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,12 +1,21 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import CartReducer from '../features/cart/cartSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+const rootReducer=combineReducers({
+    cart:CartReducer,
+})
+const persistConfig={
+    key:'root',
+    storage
+}
+
+const peristedReducer=persistReducer(persistConfig,rootReducer);
 
 export const store=configureStore({
-    reducer:{
-        cart:CartReducer
-    }
+    reducer:peristedReducer
 })
-
+export const persistor=persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
