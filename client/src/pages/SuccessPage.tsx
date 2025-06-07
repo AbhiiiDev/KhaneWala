4ad8e9195bc203a4b77dfe44c40f7398fd9bc1d3
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2025-06-08 00:24:00 +0530

    forgot to add successpage component in git

---
+++ client/src/pages/SuccessPage.tsx
@@ -0,0 +1,12 @@
+
+const SuccessPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+    <div className=' bg-green-500 rounded-full'>
+     !! Order Successfull, Hold tight we are preaparing you order !!
+    </div>
+    </div>
+  )
+}
+
+export default SuccessPage
