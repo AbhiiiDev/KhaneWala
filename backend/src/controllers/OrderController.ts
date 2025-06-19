7c568368c4dce8c576bc0f06cce2f4d6d9ab2c51
AbhiiiDev <abhishek.2125cs1088@kiet.edu>
2025-06-19 16:01:04 +0530

    Order Summary and Order checkout completed

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -6,7 +6,7 @@ const getOrder=async(req:Request,res:Response)=>{
     const orderId=req.params.orderId;
 try {
 
-    const order=await Order.findById(orderId).populate("restaurant","restaurantName city imageUrl");;
+    const order=await Order.findById(orderId).populate("restaurant","restaurantName city imageUrl deliveryPrice estimatedDeliveryTime");
     if(!order)
         return res.status(404).json({message:"Can't find any order"});
     return res.json(order);
